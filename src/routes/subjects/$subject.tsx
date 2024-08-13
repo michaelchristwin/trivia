d1bef22e7cf07b6db389f3d186900f4d66bf7baa
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-08-13 12:13:32 +0100

    Added title

--- src/routes/subjects/$subject.tsx
+++ src/routes/subjects/$subject.tsx
@@ -12,6 +12,7 @@ import { AnimatePresence, motion } from "framer-motion";
 const variants = {
   enter: (direction: number) => {
     return {
+      zIndex: 0,
       x: direction > 0 ? 1000 : -1000,
       opacity: 0,
     };
@@ -60,22 +61,27 @@ export const Route = createFileRoute("/subjects/$subject")({
 
 function Quiz() {
   const questions = Route.useLoaderData<TriviaDBResponse>();
-  const [index, setIndex] = useState(0);
-
   const data = questions.results;
   console.log(questions);
-  let choices = [...data[index].incorrect_answers, data[index].correct_answer];
-  console.log(choices);
-  choices = shuffleArray(choices);
 
   const [[page, direction], setPage] = useState([0, 0]);
-  const imageIndex = wrap(0, data.length, page);
+  const qIndex = wrap(0, data.length, page);
+  let choices = [
+    ...data[qIndex].incorrect_answers,
+    data[qIndex].correct_answer,
+  ];
 
+  choices = shuffleArray(choices);
   const paginate = (newDirection: number) => {
     setPage([page + newDirection, newDirection]);
   };
   return (
     <div className={`flex w-full h-full relative`}>
+      <p
+        className={`absolute top-[50px] left-[50%] text-[20px] font-bold underline text-secondary2 translate-x-[-50%]`}
+      >
+        {data[qIndex].category}
+      </p>
       <AnimatePresence initial={false} custom={direction}>
         <motion.div
           custom={direction}
@@ -86,27 +92,32 @@ function Quiz() {
           key={page}
           transition={{
             x: { type: "spring", stiffness: 300, damping: 30 },
-            opacity: { duration: 0.2 },
+            opacity: { duration: 0.5 },
           }}
           className={`flex w-full p-4 h-full absolute top-[200px] left-0 space-x-4`}
         >
           <div
-            className={`rounded-[8px] text-secondary2 border-2 font-bold border-secondary2 flex justify-center items-center text-[17px] w-[30px] h-[40px]`}
+            className={`rounded-full text-white bg-secondary2 font-bold flex justify-center items-center text-[17px] w-[30px] h-[30px]`}
           >
-            {index + 1}
+            {qIndex + 1}
           </div>
-          <div className={`flex flex-grow flex-col h-[80px]`}>
-            <div className={`flex-1 text-[16px]`}>{data[index].question}</div>
+          <div className={`flex flex-grow flex-col h-[80px] space-y-2`}>
+            <div
+              className={`flex-1 text-[16px]`}
+              dangerouslySetInnerHTML={{ __html: data[qIndex].question }}
+            ></div>
             <div className={`flex-1`}>
-              <ToggleGroup type="single" className={`space-x-2 justify-start`}>
+              <ToggleGroup
+                type="single"
+                className={`space-x-2 justify-start lg:w-[700px] md:w-[500px] w-[300px] flex-wrap gap-y-2`}
+              >
                 {choices.map((choice, i) => (
                   <ToggleGroupItem
                     value={choice}
                     key={i}
+                    dangerouslySetInnerHTML={{ __html: choice }}
                     className={`border border-neutral-700 data-[state=on]:bg-secondary2 data-[state=on]:border-0`}
-                  >
-                    {choice}
-                  </ToggleGroupItem>
+                  ></ToggleGroupItem>
                 ))}
               </ToggleGroup>
             </div>
@@ -114,20 +125,26 @@ function Quiz() {
         </motion.div>
       </AnimatePresence>
       <div
-        className={`flex float-end w-[130px] h-[35px] absolute top-[340px] right-[40px] items-center space-x-2`}
+        className={`flex z-10 float-end w-[130px] h-[35px] absolute top-[340px] right-[40px] items-center space-x-2`}
       >
-        <button
+        <motion.button
+          whileHover={{ scale: 1.03 }}
+          whileTap={{ scale: 0.9 }}
           type="button"
+          onClick={() => paginate(-1)}
           className={`flex-1 border rounded-[6px] h-full border-neutral-600`}
         >
           Prev
-        </button>
-        <button
+        </motion.button>
+        <motion.button
           type="button"
+          whileHover={{ scale: 1.03 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={() => paginate(1)}
           className={`flex-1 border rounded-[6px] h-full border-neutral-600`}
         >
           Next
-        </button>
+        </motion.button>
       </div>
     </div>
   );
